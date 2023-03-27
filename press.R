20ba5606eef5355577c34ae58c02110e8a070bf5
ethan-duvall <ethan0048@live.com>
2023-03-27 05:11:44 -0400

    h

---
+++ press.R
@@ -0,0 +1,34 @@
+## **Selected press releases**
+
+• USA Today. (2023, March 23). "Climate change washed away bald eagles' food, research says. Now these eagles are becoming farm birds." Retrieved from https://www.usatoday.com/story/news/nation/2023/03/18/bald-eagles-adapt-after-climate-change/11470297002/
+  
+  •	Vancouver Sun. (2023, March 18) "Climate change forcing bald eagles to new scavenging grounds, which is OK with farmers" Retrieved from https://vancouversun.com/news/climate-change-forcing-bald-eagles-new-scavenging-bc-farms
+
+•	Bellingham Herald. (2022, October 8) "Bald eagles have returned to the Nooksack in droves; why their future may be at risk" Retrieved from https://www.bellinghamherald.com/news/local/article263227218.html
+
+#Media
+
+•	Duvall, E. (2019, December 11). Eagles, Salmon, and a Free-flowing River [Blog post]. Retrieved from 
+https://www.americanrivers.org/2019/12/eagles-salmon-and-a-free-flowing-river/
+  
+  •	Duvall, E. (2020, June 20). Why save water in the PNW? [Blog post]. Retrieved from https://weneedwater.org/why-conserve-water-in-the-pnw/
+  
+  •	Speech given at the 2019 Huxley College of the Environment commencement ceremony:
+  “2019 Huxley Graduation THH Winner Ethan Duvall Speech”
+https://www.youtube.com/watch?v=t4MsRdLxKiE
+
+•	Short film about my undergraduate research, produced by Western Washington University:
+  “WWU Give Day ’19 Video| Ethan Duvall | Huxley Ecology Research”
+https://vimeo.com/336206305
+
+•	Article about my research on the Elwha River, written by the Western Front:
+  “Environmental Science Students Analyze a River Reborn”
+http://www.westernfrontonline.com/2019/01/10/environmental-science-students-analyze-a-river-reborn/
+  
+  •	Video feature about the importance of education and action in conservation issues:
+  "Education and Action - Luke Duvall #WeNeedWater"
+https://www.youtube.com/watch?v=FYlf_vfEEno&t=30s
+
+•	Video I directed promoting community engagement in keeping rivers clean:
+  "Protecting Clean Water as a COmmunity #WeNeedWater"
+https://www.youtube.com/watch?v=pmuJgfw4vSE
